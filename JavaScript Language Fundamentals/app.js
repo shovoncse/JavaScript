b2ca155fdbbb35aae405a23b72f52538b84d2d01
Shovon Das <shovondas26@gmail.com>
2019-03-05 14:03:37 +0600

    Variable

--- JavaScript Language Fundamentals/app.js
+++ JavaScript Language Fundamentals/app.js
@@ -3,7 +3,9 @@
 
 // Single line comment
 
-/* Multi Line Comment */  
+/* Multi Line Comment */
+
+/* ======= Uncomment From here
 
 // Log to Console
 console.log('Hello WOrld');
@@ -36,3 +38,70 @@ console.timeEnd('count');
 
 //Console Clear
 console.clear();
+
+=========== To There */ 
+
+/* ======================
+        VARIABLE
+=========================*/
+// var , let , const
+
+/* VAR */
+/*
+var name = 'Jhon Doe';
+console.log(name);
+name = 'Steve Smith';
+console.log(name);
+
+//Init Var
+var greeting;
+console.log(greeting);
+greeting = 'hello';
+console.log(greeting);
+
+//Letters, Numbers, _, $
+// Cann't start with number
+
+// Multi Word vars
+
+var firstName = 'Jhon'; // Camel case
+var first_name = 'Sara'; // underscore
+var FirstName = 'Sara'; // Pascal Case
+var firstname = 'abc'; //(Not Recommanded)
+*/
+
+/* LET */
+// let name;
+// name = 'Jhon Doe';
+// console.log(name);
+// name = 'Steve Smith';
+// console.log(name);
+
+/* CONST */
+// const name;
+// const name = 'Jhon';
+// name = 'Jhon Doe';
+// console.log(name);
+// name = 'Steve Smith';
+// console.log(name);
+
+// const person = {
+//     name: 'Jhon',
+//     age : 26
+// }
+
+// console.log(person);
+// person.name = 'sara' ; //Mutating
+// console.log(person);
+
+// const numbers = [1,2,3,4,5];
+// console.log(numbers);
+
+// numbers.push(6); // Mutating
+// console.log(numbers);
+
+// numbers = [1,2,5,7]; //error
+
+/* ======================
+        DATATYPES
+=========================*/
