855389d0b31043f00dd1f29d18f554ff3a844b7d
Shovon Das <shovondas26@gmail.com>
2019-03-05 12:41:36 +0600

    Basic JS Fundamentals

---
+++ JavaScript Language Fundamentals/app.js
@@ -0,0 +1,38 @@
+//Test 
+//alert(123);
+
+// Single line comment
+
+/* Multi Line Comment */  
+
+// Log to Console
+console.log('Hello WOrld');
+console.log(123);
+console.log(true);
+
+//Log From Variable
+var greeting = 'Hello';
+console.log(greeting);
+
+//Array
+console.log([1,2,3,4,5]);
+console.table([1,2,3,4,5]);
+
+//Object
+console.log({a:1, b:2});
+console.table({a:1, b:2});
+
+// Error & Warning
+console.error("Hello Error");
+console.error("Hello Error");
+
+// Execution Time Count
+console.time('count');
+    console.log("Task One");
+    console.log("Task Two");
+    console.log("Task Three");
+    console.log("Task Four");
+console.timeEnd('count');
+
+//Console Clear
+console.clear();
